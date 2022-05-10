448cc1f5daf79883fa3e58ffc67865ff2fbf872e
Zafrul097 <zafrulislamridoy@gmail.com>
2022-05-10 17:24:38 +0600

    made change3

--- src/sample/java/project/news/develop.java
+++ src/sample/java/project/news/develop.java
@@ -3,6 +3,19 @@ package sample.java.project.news;
 public class develop {
 
     int roll;
+    string address;
+
+    public develop(string address) {
+        this.address = address;
+    }
+
+    public string getAddress() {
+        return address;
+    }
+
+    public void setAddress(string address) {
+        this.address = address;
+    }
 
     public develop(int roll) {
         this.roll = roll;

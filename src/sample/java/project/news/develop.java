5cc42d2b511ff26f799a13b0f426e3a3763c80a2
Zafrul097 <zafrulislamridoy@gmail.com>
2022-05-10 17:19:36 +0600

    made change2

---
+++ src/sample/java/project/news/develop.java
@@ -0,0 +1,18 @@
+package sample.java.project.news;
+
+public class develop {
+
+    int roll;
+
+    public develop(int roll) {
+        this.roll = roll;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+}
